2225b6c7ec6b6a5e78986cda395c63915f8276ef
sunbo <sunbo_hi@qq.com>
2016-03-18 10:49:17 +0800

    concurrency

--- tools/ssh.py
+++ tools/ssh.py
@@ -3,6 +3,7 @@
 import paramiko
 import sys
 import readConfig
+import thread
 
 def ssh(ip):
 	client = paramiko.SSHClient()
@@ -29,8 +30,34 @@ def measure( repeat = 1, timelen = 10 ):
 	opfile.close()	
 
 
+def contest(conn ,ip, opfile):
+	command = 'netperf -H '+ ip
+	for i in range(0,10):
+		print(ip+":"+command)
+		stdin, stdout, stderr = conn.exec_command(command) 
+		opfile.write(ip+ ":" + command + "\n")
+		opfile.write(stdout.read())
+
+	conn.close()
+
+def concurrency():
+	ip1 = '192.168.10.200'
+	ip2 = '192.168.10.203'
+
+	client1 = ssh(ip1)
+	client2 = ssh(ip2)
+	opfile1 = open('reslut1.txt', 'w')
+	opfile2 = open('reslut2.txt', 'w')
+
+	try:
+		thread.start_new_thread(contest,(client1, ip1, opfile1))
+		thread.start_new_thread(contest,(client2, ip2, opfile2))
+	except Exception, e:
+		print "Error: unable to start thread"
+
+
 if __name__ == '__main__':
-	measure()
+	concurrency()
         
         
 
