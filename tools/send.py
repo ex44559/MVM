4ba2b50fc4b330a17aa30fd1be9b8fa25e32dc44
sunbo <sunbo.90s@gmail.com>
2016-04-07 12:35:07 +0800

    concurrent

--- tools/send.py
+++ tools/send.py
@@ -2,35 +2,43 @@
 
 import os
 import paramiko
+from multiprocessing import Process
+
+def netperf(opfile, ip):
+	client = paramiko.SSHClient()
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	client.connect( ip, 22, username='root', password='123456', timeout=20)
+
+	command = "ps -e | grep netserver && systemctl stop firewalld"
+	stdin, stdout, stderr = client.exec_command(command)
+
+	i = 0
+	for line in stdout.readlines():
+		i += 1
+
+	if i == 0:
+		netserver = "netserver"
+		stdin, stdout, stderr = client.exec_command(netserver)
+		print(ip + ":start netserver")
+
+	print("to ip %s: running netperf\n" % ip)
+	for i in range(0, 10):
+		opfile.write(os.popen("netperf -H " + ip).read())
+
+	opfile.close()
+	client.close()
 
 def main(ip_list):
 	r = 0
+	plist = []
 	for ip in ip_list:
-		client = paramiko.SSHClient()
-		client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-		client.connect( ip, 22, username='root', password='123456', timeout=20)
-
-		command = "ps -e | grep netserver && systemctl stop firewalld"
-		stdin, stdout, stderr = client.exec_command(command)
-
-		i = 0
-		for line in stdout.readlines():
-			i += 1
-
-		if i == 0:
-			netserver = "netserver"
-			stdin, stdout, stderr = client.exec_command(netserver)
-			print(ip + ":start netserver")
-
-		client.close()
-
 		opfile = open('result'+"%s" % r+'.txt', 'w')
-		print("to ip %s: running netperf\n" % ip)
-		for i in range(0, 10):
-			opfile.write(os.popen("netperf -H " + ip).read())
-
-		opfile.close()
-
+		try:
+			p = Process(target = netperf, args = (opfile, ip))
+			p.start()
+			plist.append(p)
+		except Exception, e:
+			print("cannot start thread %d" % r)
 		r += 1
 
 if __name__ == '__main__':
