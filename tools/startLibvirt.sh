e7c08953e4b4af0f72649fe2a539c72f7b7e03bb
sunbo <sunbo_hi@qq.com>
2016-10-21 16:37:42 +0800

    update startLibvirt

--- tools/startLibvirt.sh
+++ tools/startLibvirt.sh
@@ -1,2 +1,19 @@
-/usr/sbin/libvirtd  -d
-/usr/sbin/virtlogd -d
+libvirtd_status=`systemctl is-active libvirtd`
+virtlogd_status=`systemctl is-active virtlogd`
+
+if [[ libvirtd_status == "unkown" ]]; then
+	/usr/sbin/libvirtd  -d
+elif [[ libvirtd_status != "active" ]]; then
+	systemctl start libvirtd
+else
+	echo "libvirtd is running."
+fi
+
+if [[ virtlogd_status == "unkown" ]]; then
+	/usr/sbin/virtlogd  -d
+elif [[ virtlogd_status != "active" ]]; then
+	systemctl start virtlogd
+else
+	echo "libvirtd is running."
+fi
+
