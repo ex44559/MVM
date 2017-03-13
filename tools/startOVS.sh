ce3a4f4bac3e8f57f0560cdf8a2cb6c3a8ae7d45
sunbo <sunbo.90s@gmail.com>
2017-03-13 03:23:07 -0400

    add start ovs function

---
+++ tools/startOVS.sh
@@ -0,0 +1,7 @@
+`ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+                 --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+                 --private-key=db:Open_vSwitch,SSL,private_key \
+                 --certificate=db:Open_vSwitch,SSL,certificate \
+                 --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert --pidfile --detach`
+
+`ovs-vswitchd --dpdk -c 0x1 -n 4 -- --pidfile --detach`
